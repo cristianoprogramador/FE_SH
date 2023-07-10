96f719d90922a577a12f6526e6ea631e00b661bc
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-10 17:27:17 -0300

    add button and mainlayout ajust

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+import { AddButton } from "../components/addButton";
+
 export function User() {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
   return (
-    <>
-      <h1>Home Page</h1>
-      <h3>Seja bem vindo</h3>
-    </>
+    <div className="flex flex-col h-full items-center">
+      <div className="mt-9 pb-3 gap-8 align-middle items-center grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        <AddButton openModal={openModal} />
+      </div>
+    </div>
   );
 }

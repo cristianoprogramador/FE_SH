828c287cf06c3111836b65904063c3c0c38a9848
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-11 09:55:18 -0300

    create the hover

--- src/components/userCard.tsx
+++ src/components/userCard.tsx
@@ -10,7 +10,16 @@ interface UserCardProps {
 }
 
 export function UserCard(props: UserCardProps) {
+  const [showModal, setShowModal] = useState(false);
   const { data } = props;
+
+  const handleMouseEnter = () => {
+    setShowModal(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowModal(false);
+  };
 
   return (
     <Card className="h-52 w-48">

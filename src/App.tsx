f0a406f101f2015a5778c35c2a7d82de0fc7d0af
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-11 13:03:13 -0300

    separate routes (#4)
    * separate routes
    
    * div changed in app and mainlayout from const to function

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Sidebar } from "./components/sidebar";
-import { Chat } from "./pages/chat";
-import { User } from "./pages/user";
-import { Login } from "./pages/login";
-import { Projects } from "./pages/projects";
-import { Schedule } from "./pages/schedule";
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./router";
 
 export default function App() {
-  const isAuthenticated = true; // State is authenticated just in development
-
   return (
     <BrowserRouter>
-      <div className="flex h-screen">
-        {!isAuthenticated ? (
-          <Routes>
-            <Route path="/" element={<Login />} />
-          </Routes>
-        ) : (
-          <>
-            <Sidebar />
-            <div className="flex-grow">
-              <Routes>
-                <Route path="/users" element={<User />} />
-                <Route path="/projects" element={<Projects />} />
-                <Route path="/chat" element={<Chat />} />
-                <Route path="/schedule" element={<Schedule />} />
-              </Routes>
-            </div>
-          </>
-        )}
-      </div>
+      <AppRoutes />
     </BrowserRouter>
   );
 }

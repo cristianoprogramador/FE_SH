0810fb704da4408872a053b01850512f7f83eefe
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-14 17:33:54 -0300

    ajusted imageUrl

--- src/components/userCard.tsx
+++ src/components/userCard.tsx
@@ -3,7 +3,6 @@ import { Avatar } from "./avatar";
 import { Card } from "./card";
 import { ToolTip } from "./tooltip";
 import { User } from "@/interfaces/userInterface";
-import userDefaultImg from "@/assets/icons/user.svg";
 
 interface UserCardProps {
   data: User;
@@ -11,11 +10,10 @@ interface UserCardProps {
 
 export function UserCard(props: UserCardProps) {
   const { data } = props;
-  const imageUrl = props.data.imageUrl ?? userDefaultImg;
 
   return (
     <Card className="h-52 w-48">
-      <Avatar imageUrl={imageUrl} className="h-16 w-16" />
+      <Avatar imageUrl={data?.imageUrl} className="h-16 w-16" />
       <div className="mt-4 font-semibold gap-1 text-center">
         <div>{data.name}</div>
         <div>{data.position}</div>

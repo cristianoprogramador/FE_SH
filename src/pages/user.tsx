4f18a2c584e27e04706781eb7854ab1bd8f0d155
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-17 21:54:25 -0300

    ajusting with closeModal

--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -114,6 +114,10 @@ export function User() {
     setManageUserModalVisibility(false);
   };
 
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div className="flex flex-col h-full items-center">
       <div className="flex flex-row mt-9 justify-center gap-8 w-full">

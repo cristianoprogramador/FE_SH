e8d95a370a5da73f90236aa571708cb4b1fed18d
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-18 14:53:29 -0300

    input created

---
+++ src/components/input.tsx
@@ -0,0 +1,32 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+import { twJoin } from "tailwind-merge";
+
+interface FormInputProps {
+  label: string;
+  register: UseFormRegisterReturn;
+  error?: string;
+  className?: string;
+  placeholder?: string;
+}
+
+const INPUT_CLASSES = "border border-gray-300 p-2 rounded";
+
+export function Input(props: FormInputProps) {
+  const { label, register, error, className, placeholder } = props;
+
+  const inputWithExternalClasses = twJoin(INPUT_CLASSES, className);
+
+  return (
+    <div className="mb-4">
+      <div className="flex flex-row items-center gap-4 justify-between">
+        <label className="block">{label}:</label>
+        <input
+          {...register}
+          className={inputWithExternalClasses}
+          placeholder={placeholder}
+        />
+      </div>
+      {error && <span className="text-red-500 font-bold">{error}</span>}
+    </div>
+  );
+}

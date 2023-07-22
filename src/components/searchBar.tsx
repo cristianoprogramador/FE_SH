86f03853284b50c57d403e97ef69e409da4e1a4f
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-22 15:54:52 -0300

    SFH-18: SearchBar input (#13)
    * ajusted the imageUrl to the parent component and changed the name to userDefaultImg, also remove the console.log
    
    * ajusted imageUrl
    
    * create the hover
    
    * separate the tooltip and fix the avatar style
    
    * search input created
    
    * delete the import for useState
    
    * ajusted the name for inputWithExternalClasses and userData
    
    * ajusted with useMemo

---
+++ src/components/searchBar.tsx
@@ -0,0 +1,30 @@
+import { twJoin } from "tailwind-merge";
+
+interface SearchBarProps {
+  className?: string;
+  onSearchTermChange: (term: string) => void;
+}
+
+const BASE_CLASSES = `p-2 border border-gray-500 rounded-lg bg-transparent text-black placeholder:text-black w-80 pl-7 shadow-md`;
+
+export function SearchBar(props: SearchBarProps) {
+  const { className, onSearchTermChange } = props;
+
+  const inputWithExternalClasses = twJoin(BASE_CLASSES, className);
+
+  const handleSearchTermChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const term = event.target.value;
+    onSearchTermChange(term);
+  };
+
+  return (
+    <input
+      type="text"
+      placeholder="Pesquisar..."
+      className={inputWithExternalClasses}
+      onChange={handleSearchTermChange}
+    />
+  );
+}

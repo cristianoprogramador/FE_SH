82baad98e661f443108f274b9e50c324e71696ff
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-25 15:22:19 -0300

    first implementation

--- src/components/userCard.tsx
+++ src/components/userCard.tsx
@@ -4,13 +4,83 @@ import { Avatar } from "./avatar";
 import { Card } from "./card";
 import { ToolTip } from "./tooltip";
 import { User } from "@/interfaces/userInterface";
+import { useState } from "react";
+import { Modal } from "./modal";
+import { Input } from "./input";
+import { Button } from "./button";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { UserForm } from "@/pages/user";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 interface UserCardProps {
   data: User;
 }
 
+const projects = [
+  "Create your Burger",
+  "Laughing and Learning",
+  "My Life Dashboard",
+];
+
 export function UserCard(props: UserCardProps) {
   const { data } = props;
+  const [manageUserModalVisibility, setManageUserModalVisibility] =
+    useState(false);
+
+  const [formData, setFormData] = useState<UserForm>({
+    name: data.name,
+    email: data.email,
+    birthDate: data.birthDate,
+    position: data.position,
+    imageUrl: data.imageUrl,
+    salary: data.salary,
+    projects: data.projects ?? [],
+  });
+  const [selectedProjects, setSelectedProjects] = useState<string[]>([]);
+
+  const openEditUserModal = () => {
+    setManageUserModalVisibility(true);
+    setSelectedProjects(data.projects || []); // Definindo os projetos selecionados
+  };
+
+  const closeModal = () => {
+    setManageUserModalVisibility(false);
+  };
+
+  const schema = yup.object().shape({
+    name: yup.string().required("O nome é obrigatório"),
+    email: yup
+      .string()
+      .email("Digite um e-mail válido")
+      .required("O e-mail é obrigatório"),
+    position: yup.string().required("O cargo é obrigatório"),
+    imageUrl: yup.string().url("Digite uma URL válida"),
+    salary: yup
+      .number()
+      .transform((value) =>
+        Number.isNaN(value) ? undefined : (value as number | undefined)
+      )
+      .required("O salário é obrigatório"),
+    projects: yup.array().of(yup.string().oneOf(projects)),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      projects: [],
+    },
+  });
+
+  const onSubmit = (data: UserForm) => {
+    console.log(data);
+
+    const selectedProjectsFromForm = data.projects || [];
+  };
 
   return (
     <Card className="h-52 w-48 relative">
@@ -19,6 +89,7 @@ export function UserCard(props: UserCardProps) {
         src={editUser}
         alt="edit user"
         className="absolute right-3 top-3 cursor-pointer h-6 w-6"
+        onClick={openEditUserModal}
       />
       <div className="mt-4 font-semibold gap-1 text-center">
         <div>{data.name}</div>
@@ -43,6 +114,79 @@ export function UserCard(props: UserCardProps) {
           </ToolTip>
         </div>
       </div>
+      {manageUserModalVisibility && (
+        <Modal closeModal={closeModal}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <p className="text-center font-bold mb-5 text-lg">
+              Informações Gerais
+            </p>
+            <Input
+              label="Nome"
+              register={register("name")}
+              error={errors.name?.message}
+              defaultValue={data.name}
+            />
+            <Input
+              label="Email"
+              register={register("email")}
+              error={errors.email?.message}
+              defaultValue={data.email}
+            />
+            <Input
+              label="Cargo"
+              register={register("position")}
+              error={errors.position?.message}
+              defaultValue={data.position}
+            />
+            <Input
+              label="Foto (URL)"
+              register={register("imageUrl")}
+              error={errors.imageUrl?.message}
+              defaultValue={data.imageUrl}
+            />
+            <Input
+              label="Salário"
+              register={register("salary")}
+              error={errors.salary?.message}
+              defaultValue={data.salary}
+              type="number"
+              min={0}
+            />
+            <div className="mb-4">
+              <div className="flex flex-row items-center gap-4 justify-between">
+                <label className="block">Projetos:</label>
+                <div className="flex flex-col max-h-40 overflow-y-auto">
+                  {projects.map((project) => (
+                    <div key={project} className="flex flex-row  items-center">
+                      <input
+                        type="checkbox"
+                        {...register("projects")}
+                        value={project}
+                        id={project}
+                        className="mr-2"
+                        defaultChecked={selectedProjects.includes(project)}
+                      />
+                      <label htmlFor={project}>{project}</label>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-row justify-around">
+              <Button
+                children="Fechar"
+                onClick={closeModal}
+                className="bg-ButtonBgRed text-white font-semibold hover:bg-ButtonHoverBgRed"
+              />
+              <Button
+                children="Salvar"
+                type="submit"
+                className="bg-ButtonBgGreen text-white font-semibold hover:bg-ButtonHoverBgGreen"
+              />
+            </div>
+          </form>
+        </Modal>
+      )}
     </Card>
   );
 }

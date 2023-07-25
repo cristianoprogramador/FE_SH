88274956fb9baf4cb7117b9b4f701ddb10b22788
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2023-07-25 08:14:27 -0300

    SFH-22: Input Form (#15)
    * first version of the modal
    
    * ajusting with closeModal
    
    * input created
    
    * removed short circuit sintax to full ternary
    
    * input ajusted with html attributes
    
    * ajusted input
    
    * removed value

---
+++ src/components/input.tsx
@@ -0,0 +1,39 @@
+import { UseFormRegisterReturn } from "react-hook-form";
+import { twJoin } from "tailwind-merge";
+
+interface InputCustomProps {
+  label: string;
+  register: UseFormRegisterReturn;
+  error?: string;
+}
+
+type InputProps = Pick<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  "className" | "placeholder" | "min" | "type"
+> &
+  InputCustomProps;
+
+const INPUT_CLASSES = "border border-gray-300 p-2 rounded";
+
+export function Input(props: InputProps) {
+  const { label, register, error, className, ...restOfInputProps } = props;
+  const hasError = error != null;
+
+  const inputWithExternalClasses = twJoin(INPUT_CLASSES, className);
+
+  return (
+    <div className="mb-4">
+      <div className="flex flex-row items-center gap-4 justify-between">
+        <label className="block">{label}:</label>
+        <input
+          {...register}
+          className={inputWithExternalClasses}
+          {...restOfInputProps}
+        />
+      </div>
+      {hasError ? (
+        <span className="text-red-500 font-bold">{error}</span>
+      ) : null}
+    </div>
+  );
+}
